6cda8e276203151377ec979104a8a2ae2c5a2706
142502105 <142502105@M1-ADOBE06>
2025-10-27 09:49:18 +0200

    etadpu eht

---
+++ Gadgets/Update.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Gadgets
+{
+    public partial class Update : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (GridView1.Rows.Count > 0)
+            {
+                txsearch.Text = GridView1.Rows[0].Cells[0].Text;
+                txsearch0.Text = GridView1.Rows[0].Cells[0].Text;
+                txsearch1.Text = GridView1.Rows[0].Cells[1].Text;
+                txsearch2.Text = GridView1.Rows[0].Cells[2].Text;
+                txsearch3.Text = GridView1.Rows[0].Cells[3].Text;
+                txsearch4.Text = GridView1.Rows[0].Cells[4].Text;
+                txsearch5.Text = GridView1.Rows[0].Cells[5].Text;
+                txsearch6.Text = GridView1.Rows[0].Cells[6].Text;
+                txsearch7.Text = GridView1.Rows[0].Cells[7].Text;
+                txsearch8.Text = GridView1.Rows[0].Cells[8].Text;
+            }
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.Update();
+        }
+    }
+}
